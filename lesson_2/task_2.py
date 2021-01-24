5134855d8e3114b4244bd9e49120a4b7f34ecf74
oleg_dm <i@horo.so>
2021-01-25 01:44:45 +0300

    first step

---
+++ lesson_2/task_2.py
@@ -0,0 +1,19 @@
+"""
+2. Посчитать четные и нечетные цифры введенного натурального числа.
+ Например, если введено число 34560, в нем 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
+"""
+
+
+def nut_num(into, reg=0, irreg=0):
+    if (into % 10) % 2 == 0:
+        reg += 1
+    else:
+        irreg += 1
+    if len(str(into)) > 1:
+        into = into // 10
+        return nut_num(into, reg, irreg)
+    else:
+        return f'Чётных: {reg}, нечётных {irreg}'
+
+
+print(nut_num(34560))
---
+++ lesson_2/task_3.py
@@ -0,0 +1,20 @@
+"""
+3. Сформировать из введенного числа обратное по порядку входящих в него цифр и вывести на экран.
+Например, если введено число 3486, надо вывести 6843.
+"""
+
+
+def revers(num_in, tmp=None, cnt=None):
+    if cnt == 1:
+        return tmp
+    elif cnt is None:
+        cnt = len(str(num_in))
+    else:
+        cnt -= 1
+    tmp = num_in % 10 if tmp is None else int(str(tmp) + str(num_in % 10))
+    return revers(num_in // 10, tmp, cnt)
+
+
+print(revers(348060))
+print(revers(3))
+print(revers(304))
